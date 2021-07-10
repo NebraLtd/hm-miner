6f5f5cb4a9bd29ce3d1ef69f151f64471f084f16
Viktor Petersson <self@vpetersson.com>
2021-07-10 19:39:50 +0100

    Switches to new blessed block location

--- start-miner.sh
+++ start-miner.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-wget -O /opt/miner/releases/0.1.0/sys.config https://hmapi.nebra.com/docker.config
+wget \
+    -O /opt/miner/releases/0.1.0/sys.config \
+    https://helium-assets.nebra.com/docker.config
 
 PUBLIC_KEYS=$(/opt/miner/bin/miner print_keys)
 [ $? -ne 0 ] && exit 1
-echo $PUBLIC_KEYS > /var/data/public_keys
+echo "$PUBLIC_KEYS" > /var/data/public_keys
 
 /opt/miner/gen-region.sh &
 
