c0facc657d4ffc61fcff1b8e052501d6c2e49dd0
Aaron Shaw <shawaj@gmail.com>
2021-08-01 14:56:49 +0100

    fix: error SC2086
    fix shellcheck error SC2086

--- start-miner.sh
+++ start-miner.sh
@@ -6,7 +6,7 @@ wget \
 
 PUBLIC_KEYS=$(/opt/miner/bin/miner print_keys)
 [ $? -ne 0 ] && exit 1
-echo $PUBLIC_KEYS > /var/data/public_keys
+echo "$PUBLIC_KEYS" > /var/data/public_keys
 
 /opt/miner/gen-region.sh &
 
