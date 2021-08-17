2a30bf9a3351ad130754082455478a5241c42ace
Robert Putt <robe8437@Roberts-MacBook-Pro.local>
2021-08-17 09:43:01 +0100

    Allow override of config url for dev / debug / experimentation purposes

--- start-miner.sh
+++ start-miner.sh
@@ -2,7 +2,7 @@
 
 wget \
     -O /opt/miner/releases/0.1.0/sys.config \
-    https://helium-assets.nebra.com/docker.config
+    "${OVERRIDE_CONFIG_URL:=https://helium-assets.nebra.com/docker.config}"
 
 if ! PUBLIC_KEYS=$(/opt/miner/bin/miner print_keys)
 then
