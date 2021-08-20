2fdfa76de1a398c0a876075aced1e0332ca211f1
Viktor Petersson <self@vpetersson.com>
2021-08-20 11:46:03 +0100

    Bring back change

--- start-miner.sh
+++ start-miner.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-# Temporarily disable pulling down config
-#wget \
-#    -O /opt/miner/releases/0.1.0/sys.config \
-#    "${OVERRIDE_CONFIG_URL:=https://helium-assets.nebra.com/docker.config}"
+wget \
+    -O /opt/miner/releases/0.1.0/sys.config \
+    "${OVERRIDE_CONFIG_URL:=https://helium-assets.nebra.com/docker.config}"
 
 if ! PUBLIC_KEYS=$(/opt/miner/bin/miner print_keys)
 then
