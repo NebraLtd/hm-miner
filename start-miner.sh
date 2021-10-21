7470f3daa7cb9674598ec7014187d4de8fc48e36
Aaron Shaw <shawaj@gmail.com>
2021-10-21 17:06:43 +0100

    remove public key function
    removing public key function again in preparation for other fixes in diag and config and re-removal of gwmfr

--- start-miner.sh
+++ start-miner.sh
@@ -10,19 +10,12 @@ wget \
     -O "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config" \
     "${OVERRIDE_CONFIG_URL:=https://helium-assets.nebra.com/docker.config}"
 
-# Wait for the diagnostics app is loaded
+# Wait for the diagnostics app to be loaded
 until wget -q -T 10 -O - http://diagnostics:5000/initFile.txt > /dev/null 2>&1
 do
     echo "Diagnostics container not ready. Going to sleep."
     sleep 10
 done
-
-if ! PUBLIC_KEYS=$(/opt/miner/bin/miner print_keys)
-then
-  exit 1
-else
-  echo "$PUBLIC_KEYS" > /var/data/public_keys
-fi
 
 /opt/miner/gen-region.sh &
 
