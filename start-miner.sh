58b3deec306a703ed403f29e5ddb6b5694e9ef82
Marvin Arnold <marvin@geeky.rocks>
2022-03-02 11:09:03 -0600

    feat: use nebracdn for default sys.config

--- start-miner.sh
+++ start-miner.sh
@@ -7,14 +7,14 @@ if [ "$(df -h /var/data/ | tail -1 | awk '{print $5}' | tr -d '%')" -ge 80 ]; th
 fi
 
 # Set OVERRIDE_CONFIG_URL based on BALENA_DEVICE_TYPE
-OVERRIDE_CONFIG_URL="${RASPBERRYPI_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config}"
+OVERRIDE_CONFIG_URL="${RASPBERRYPI_MINER_CONFIG_URL:-https://helium-assets.nebracdn.com/docker.config}"
 
 if [ "$BALENA_DEVICE_TYPE" = "rockpi-4b-rk3399" ]; then
   cp /opt/miner/docker.config.rockpi "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config"
-  OVERRIDE_CONFIG_URL="${ROCKPI_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config.rockpi}"
+  OVERRIDE_CONFIG_URL="${ROCKPI_MINER_CONFIG_URL:-https://helium-assets.nebracdn.com/docker.config.rockpi}"
 elif [ "$BALENA_DEVICE_TYPE" = "intel-nuc" ]; then
   cp /opt/miner/docker.config.5g "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config"
-  OVERRIDE_CONFIG_URL="${5G_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config.5g}"
+  OVERRIDE_CONFIG_URL="${5G_MINER_CONFIG_URL:-https://helium-assets.nebracdn.com/docker.config.5g}"
 fi
 
 wget \
