b103daf39ba03b3895cf9d5af5526e7155eff7cd
Marvin Arnold <marvin@geeky.rocks>
2022-08-25 06:59:49 -0500

    wait the whole thing

--- start-miner.sh
+++ start-miner.sh
@@ -1,43 +1,51 @@
-#!/usr/bin/env sh
-
-. ./dbus-wait.sh
-
-if [ "$(df -h /var/data/ | tail -1 | awk '{print $5}' | tr -d '%')" -ge 80 ]; then
-    rm -rf /var/data/*
-fi
-
-# Set OVERRIDE_CONFIG_URL based on BALENA_DEVICE_TYPE
-OVERRIDE_CONFIG_URL="${RASPBERRYPI_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config}"
-
-if [ "$BALENA_DEVICE_TYPE" = "rockpi-4b-rk3399" ]; then
-  cp /opt/miner/docker.config.rockpi "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config"
-  OVERRIDE_CONFIG_URL="${ROCKPI_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config.rockpi}"
-elif [ "$BALENA_DEVICE_TYPE" = "intel-nuc" ]; then
-  cp /opt/miner/docker.config.5g "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config"
-  OVERRIDE_CONFIG_URL="${5G_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config.5g}"
-elif [ "$VARIANT" = "COMP-PISCESP100" ]; then
-  cp /opt/miner/docker.config.pisces "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config"
-  OVERRIDE_CONFIG_URL="${ROCKPI_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config.pisces}"
-fi
-
-wget \
-    -O "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config" \
-    "${OVERRIDE_CONFIG_URL}"
-
-# Wait for the diagnostics app to be loaded
-until wget -q -T 10 -O - http://diagnostics:5000/initFile.txt > /dev/null 2>&1
-do
-    echo "Diagnostics container not ready. Going to sleep."
-    sleep 10
-done
-
-/opt/miner/gen-region.sh &
-
+echo "Starting miner container"
 prevent_start="${PREVENT_START_MINER:-0}"
 if [ "$prevent_start" = 1 ]; then
     echo "Miner process will not be started. PREVENT_START_MINER=1"
     while true; do sleep 1000; done
 else
-    wait_for_dbus \
-    && /opt/miner/bin/miner foreground
+    #!/usr/bin/env sh
+
+    echo "DBUS wait"
+    . ./dbus-wait.sh
+
+
+    echo "Removing var/data"
+    if [ "$(df -h /var/data/ | tail -1 | awk '{print $5}' | tr -d '%')" -ge 80 ]; then
+        rm -rf /var/data/*
+    fi
+
+    echo "Copying configs"
+    # Set OVERRIDE_CONFIG_URL based on BALENA_DEVICE_TYPE
+    OVERRIDE_CONFIG_URL="${RASPBERRYPI_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config}"
+
+    if [ "$BALENA_DEVICE_TYPE" = "rockpi-4b-rk3399" ]; then
+    cp /opt/miner/docker.config.rockpi "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config"
+    OVERRIDE_CONFIG_URL="${ROCKPI_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config.rockpi}"
+    elif [ "$BALENA_DEVICE_TYPE" = "intel-nuc" ]; then
+    cp /opt/miner/docker.config.5g "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config"
+    OVERRIDE_CONFIG_URL="${5G_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config.5g}"
+    elif [ "$VARIANT" = "COMP-PISCESP100" ]; then
+    cp /opt/miner/docker.config.pisces "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config"
+    OVERRIDE_CONFIG_URL="${ROCKPI_MINER_CONFIG_URL:-https://helium-assets.nebra.com/docker.config.pisces}"
+    fi
+
+    echo "Downloading sysconfig"
+    wget \
+        -O "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config" \
+        "${OVERRIDE_CONFIG_URL}"
+
+    echo "Waiting for diagnostics"
+    # Wait for the diagnostics app to be loaded
+    until wget -q -T 10 -O - http://diagnostics:5000/initFile.txt > /dev/null 2>&1
+    do
+        echo "Diagnostics container not ready. Going to sleep."
+        sleep 10
+    done
+
+    echo "Gen region"
+    /opt/miner/gen-region.sh &
+
+    echo "Dbus wait"
+    wait_for_dbus  && /opt/miner/bin/miner foreground
 fi
