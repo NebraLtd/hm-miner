92a8ab91cac659cdc4b69d3c936a6620dad8a2d6
pritam.ghanghas@gmail.com <pritam.ghanghas@gmail.com>
2022-11-09 12:59:52 +0530

    feat: Update startup script for dual frequency plans#110
    * continously update region file every 60 seconds.

--- gen-region.sh
+++ gen-region.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env sh
 
-# Wait until miner knows the regulatory region.
-while ! /opt/miner/bin/miner info region > /dev/null 2>&1; do
-    sleep 1
+# we want to check and update region file forever
+while :
+do
+    # Wait until miner knows the regulatory region, we don't want to 
+    # write some random strings to region file
+    while ! /opt/miner/bin/miner info region > /dev/null 2>&1; do
+        sleep 1
+    done
+
+    # update region file every 60 seconds
+    REGIONDATA=$(/opt/miner/bin/miner info region)
+    echo "$REGIONDATA" > /var/pktfwd/region
+    sleep 60
 done
 
-REGIONDATA=$(/opt/miner/bin/miner info region)
-echo "$REGIONDATA" > /var/pktfwd/region
